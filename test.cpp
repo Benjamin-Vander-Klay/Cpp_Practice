260425273e13d886ab0fe272eda4ddd560986980
BVK_TP_X230 <benjaminvanderklay@gmail.com>
2024-10-17 20:16:45 -0700

    got vscode up on X230. Starting some cpp practice

---
+++ test.cpp
@@ -0,0 +1,34 @@
+/*
+* Author: BVK
+* Date: 10/17/2024 -Ava's bday
+* File: test.cpp 
+* Notes: de-rusting
+* 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// #include test.h // our own header file
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    //start with some C, then ++
+
+    // how do we access the CLArgs again?
+
+    // for(int i = 0; i < argc; i++){
+        //as many arguments as there are:
+
+    // }
+    printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+    printf("\r\n Hello Linux\r\n Compiled on %s at %s\r\n\r\n", __DATE__, __TIME__);
+
+
+        
+
+    printf("\r\nExiting Program...\r\n");
+    return 0;// or while(1); first if embedded
+}
